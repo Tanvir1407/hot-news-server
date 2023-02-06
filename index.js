1f709a4719408134c1f987e1de3792ee7dae5c78
MD. Tanvir <mt14050@gmail.com>
2023-02-06 17:17:25 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+
+const categories = require('./data/Categories.json');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
